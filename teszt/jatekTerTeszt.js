4526e3376057ae0af62c7491b4359fadfb5fc7cb
Bori888 <ilyes.bori8@gmail.com>
2025-04-02 14:09:45 +0200

    first commit

---
+++ teszt/jatekTerTeszt.js
@@ -0,0 +1,15 @@
+import Jatekter from "../Jatekter.js"
+/*TEsztesete
+0.Még fojik a játék de nincs nyertes
+1.dontetlen
+2.goztes vizszintes X
+3.gyoztes fugoleges X
+4.gyoztes atlos     X
+5.goztes vizszintes O
+6.gyoztes fugoleges O
+7.gyoztes atlos     O
+
+ */
+function tesztGyozelemEllenorzes_teszteset0() {
+    
+}
