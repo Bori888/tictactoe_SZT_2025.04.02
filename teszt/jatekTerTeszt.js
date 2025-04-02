d33be2f9055ab5981b6562b2e3e68a5a89b20375
Bori888 <ilyes.bori8@gmail.com>
2025-04-02 14:29:47 +0200

    kész a 2. teszt a döntetelen

--- teszt/jatekTerTeszt.js
+++ teszt/jatekTerTeszt.js
@@ -10,6 +10,61 @@ import Jatekter from "../Jatekter.js"
 7.gyoztes atlos     O
 
  */
-function tesztGyozelemEllenorzes_teszteset0() {
+function tesztGyozelemEllenorzes_nincsNyertes() {
+    /*létrehozok egy vizuáális játékteret Mock elem */
+    const szElem =document.createElement("div")
+    const jatekter = new Jatekter(szElem)
+    console.log(jatekter);
+
+    const e  = new CustomEvent("kivalaszt",{detail:0})
+    window.dispatchEvent(e)
+
+    const e2  = new CustomEvent("kivalaszt",{detail:3})
+    window.dispatchEvent(e2)
+
+    const e3  = new CustomEvent("kivalaszt",{detail:5})
+    window.dispatchEvent(e3)
+    let vart = "-"
+    let kapott = jatekter.gyozelemEllenorzes()
+    console.assert(kapott===vart,`várt:${vart} kapott:${kapott}`,"Hiba:Nem nyert még senki nem is döntetlen")
+    console.log("lefutott a nincsNyertes--Teszt()");
+    
+    
     
 }
+function tesztGyozelemEllenorzes_dontetlen() {
+    /*létrehozok egy vizuáális játékteret Mock elem */
+    const szElem =document.createElement("div")
+    const jatekter = new Jatekter(szElem)
+    console.log(jatekter);
+
+    const e1  = new CustomEvent("kivalaszt",{detail:0})
+    window.dispatchEvent(e1)
+    const e2  = new CustomEvent("kivalaszt",{detail:2})
+    window.dispatchEvent(e2)
+    const e3  = new CustomEvent("kivalaszt",{detail:1})
+    window.dispatchEvent(e3)
+    const e4  = new CustomEvent("kivalaszt",{detail:3})
+    window.dispatchEvent(e4)
+    const e5  = new CustomEvent("kivalaszt",{detail:5})
+    window.dispatchEvent(e5)
+    const e6  = new CustomEvent("kivalaszt",{detail:4})
+    window.dispatchEvent(e6)
+    const e7  = new CustomEvent("kivalaszt",{detail:6})
+    window.dispatchEvent(e7)
+    const e8  = new CustomEvent("kivalaszt",{detail:7})
+    window.dispatchEvent(e8)
+    const e9  = new CustomEvent("kivalaszt",{detail:8})
+    window.dispatchEvent(e9)
+
+    
+    let vart = "-"
+    let kapott = jatekter.gyozelemEllenorzes()
+    console.assert(kapott===vart,`várt:${vart} kapott:${kapott}`,"Hiba:döntetlen")
+    console.log("lefutott a Döntetelen --Teszt()");
+    
+    
+    
+}
+tesztGyozelemEllenorzes_nincsNyertes()
+tesztGyozelemEllenorzes_dontetlen()
